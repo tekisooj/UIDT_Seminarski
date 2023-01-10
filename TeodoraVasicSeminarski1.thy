b58510c50ca877a70464c377b5d33372383b0a62
tekisooj <72854262+tekisooj@users.noreply.github.com>
2023-01-10 19:09:17 +0100

    Update TeodoraVasicSeminarski1.thy

--- TeodoraVasicSeminarski1.thy
+++ TeodoraVasicSeminarski1.thy
@@ -3,9 +3,9 @@ theory TeodoraVasicSeminarski1
 
 begin
 
-lemma seminarski1:
-  fixes n::nat and k::nat and m::nat
-  assumes "n \<noteq> 1 " " (n^k + m*n^l + 1) dvd (n^(k+l)-1)"
-  shows "(m = 1 \<and> l=2*k) \<and> ((l dvd k)\<or>(m*(n^l - 1) = (n ^(k-l)-1)))"
+lemma help1:
+  fixes n m k l::nat
+  assumes "k > 0""m > 0""l > 0""n > 1""(n^k + m*n^l + 1) dvd (n^(k+l) - 1)"
+  shows "(n^k + m*n^l + 1) dvd (n^(k+l) + n^k + m*n^l)"
   sorry
 
